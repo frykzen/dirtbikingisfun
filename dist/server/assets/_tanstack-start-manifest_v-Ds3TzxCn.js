bc885ea9bd136b997550082e123b4637bb0f5acd
Frykzen <daniel.nikov.2010@gmail.com>
2026-05-20 21:25:20 +0300

    fix netlify build

---
+++ dist/server/assets/_tanstack-start-manifest_v-Ds3TzxCn.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Moni/Desktop/yooo_updated/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/dirtbikingisfun/assets/index-BKgY35nN.js"] }, "/": { filePath: "C:/Users/Moni/Desktop/yooo_updated/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/dirtbikingisfun/assets/index-BhBAWffm.js"] } }, clientEntry: "/dirtbikingisfun/assets/index-BKgY35nN.js" });
+export {
+  tsrStartManifest
+};
